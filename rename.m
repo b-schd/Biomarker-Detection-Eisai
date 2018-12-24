c7197f1436fecfa006cb5f35b2fce430381b835d
hoameng <hoameng@upenn.edu>
2018-12-23 17:06:48 -0800

    test

--- rename.m
+++ rename.m
@@ -1,10 +1,21 @@
-fn = dir('Jensen_Eisai*0.mat');
+fn = dir('Jensen_Eisai*LL*.mat');
 
 for i =1:numel(fn)
     curfn = fn(i).name;
     t = load(curfn);
     feat = t.feat;
     curfn = strrep(curfn,'LL-global','LL');
+    fprintf('%s\n',curfn)
+    save(curfn,'feat')
+end
+
+fn = dir('Jensen_Eisai*freq*.mat');
+
+for i =1:numel(fn)
+    curfn = fn(i).name;
+    t = load(curfn);
+    feat = t.feat;
+    curfn = strrep(curfn,'freq-global','freq');
     fprintf('%s\n',curfn)
     save(curfn,'feat')
 end
