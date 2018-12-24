2a2082f5f5a78bd5862c9b079a6e147bc48c2dbe
hoameng <hoameng@upenn.edu>
2018-12-23 17:39:13 -0800

    test

--- calc_tfeatures.m
+++ calc_tfeatures.m
@@ -6,6 +6,6 @@ function feats = calc_tfeatures(data, fs)
     ZCFn = @(x) sum((x(1:end-1,:)>repmat(mean(x),size(x,1)-1,1)) & x(2:end,:)<repmat(mean(x),size(x,1)-1,1) | (x(1:end-1,:)<repmat(mean(x),size(x,1)-1,1) & x(2:end,:)>repmat(mean(x),size(x,1)-1,1)));
     LLFn = @(x,fs) nanmean(abs(diff(x)));
 
-    feats = [AreaFn(data) EnergyFn(data) ZCFn(data) LLFN(data,fs)];
+    feats = [AreaFn(data) EnergyFn(data) ZCFn(data) LLFn(data,fs)];
     
 end
